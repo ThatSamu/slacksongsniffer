53f992ad19bdbdc6c968ad400bc9aea276cc5891
ThatSamu <samuaapa@gmail.com>
2017-02-04 15:10:09 +0200

    Added authorization

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ var SnifferBot = require('./lib/snifferbot.js');
 var token = process.env.BOT_API_KEY;
 var playlistID = process.env.PLAYLIST_ID;
 var playlistOwner = process.env.PLAYLIST_OWNER;
+var spotifyClientID = process.env.SPOTIFY_CLIENT_ID;
+var spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+var spotifyRedirectURI = process.env.SPOTIFY_REDIRECT_URI;
 
 var snifferbot = new SnifferBot({
     token: token,
     playlistID: playlistID,
-    playlistOwner: playlistOwner
+    playlistOwner: playlistOwner,
+    spotifyClientID: spotifyClientID,
+    spotifyClientSecret: spotifyClientSecret,
+    spotifyRedirectURI: spotifyRedirectURI
 });
 
 snifferbot.run();
