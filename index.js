3656dd5a1985e3e99c9bd2c617d60cb7a3998fa0
ThatSamu <samuaapa@gmail.com>
2017-02-04 14:38:17 +0200

    fixed start issue

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var SnifferBot = require('./lib/snifferbot.js');
 
 var token = process.env.BOT_API_KEY;
-var name = process.env.BOT_NAME;
+var playlistID = process.env.PLAYLIST_ID;
+var playlistOwner = process.env.PLAYLIST_OWNER;
 
 var snifferbot = new SnifferBot({
     token: token,
-    name: name
+    playlistID : playlistID,
+    playlistOwner = playlistOwner
 });
 
 snifferbot.run();
