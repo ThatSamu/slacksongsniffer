56f1342a22877f6abaaaa409e3ea95f9c5b117ca
ThatSamu <samuaapa@gmail.com>
2017-06-21 08:08:57 +0300

    added listening on port to get rid of timeout
    q
    z

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ var snifferbot = new SnifferBot({
     spotifyRedirectURI: spotifyRedirectURI
 });
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 snifferbot.run();
